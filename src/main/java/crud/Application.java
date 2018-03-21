e99bd47d00c390187d7deba092a379d7b3788493
Dziarmaga <e-mida@gft.com>
2018-03-21 11:02:17 +0100

    static content

--- src/main/java/crud/Application.java
+++ src/main/java/crud/Application.java
@@ -2,9 +2,11 @@ package crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-public class Application {
+@Configuration
+public class Application{
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
